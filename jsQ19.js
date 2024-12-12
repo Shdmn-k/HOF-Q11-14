540ed5f58be101ab95de9c532687f0a8ab9206f2
Shdmn-k <shadmeenbk@gmail.com>
2024-12-12 15:30:50 +0530

    q19 how js works assignment

---
+++ jsQ19.js
@@ -0,0 +1,14 @@
+let age = 25;
+
+function displayAge() {
+    console.log("Current age is", age);
+}
+
+function changeAge() {
+    age = 30; 
+       console.log("Updating of age done");
+}
+
+displayAge(); 
+changeAge(); 
+displayAge(); 
